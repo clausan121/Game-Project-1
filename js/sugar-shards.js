11b7fe6e0ecc0f835b5f7a04e076ddfdf02c09dc
Claudia Sanchez <claudiasanchez@Claudias-MacBook-Pro.local>
2018-04-20 09:51:21 -0400

    objects falling cat moving

--- js/sugar-shards.js
+++ js/sugar-shards.js
@@ -1,8 +1,8 @@
 // sugar shard variables
-var startingScore = 0;
-var continueAnimating = false;
-var score;
-var totalsugarShards;
+// var startingScore = 0;
+// var continueAnimating = false;
+// var score;
+// var totalsugarShards;
 var shardsArray = [];
 var shardImage = new Image();
 shardImage.src = "./images/sugarshard.png";
@@ -24,83 +24,19 @@ function addsugarShard() {
     thesugarShard.x = rando;
     shardsArray.push(thesugarShard);
 }
-
+// delete second shard.y
 function fallingShard (shard) {
-    console.log("falling");
     var id = setInterval(function(){
         shard.y+=8
-        if(y > 1000){
+        if(shard.y > 1000){
             clearInterval(id)
         }
     }, 100)
 }
 
 function drawAllTheShards(){
+    console.log("falling");
     shardsArray.forEach(function(oneShard){
         ctx.drawImage(shardImage, oneShard.x, oneShard.y,50,50)
     })
 }
-
-
-
-// function animate() {
-
-//     // request another animation frame
-
-//     if (continueAnimating) {
-//         requestAnimationFrame(animate);
-//     }
-
-//     // for each shard
-//     // (1) check for collisions
-//     // (2) advance the shard
-//     // (3) if the shard falls below the canvas, reset that shard
-
-//     for (var i = 0; i < sugarShard.length; i++) {
-
-//         var sugarShard = sugarShard[i];
-
-//         // test for cupcake-cat collision
-//         // need to define kittyCat
-
-//         if (isColliding(sugarShard, kittyCat )) {
-//             score -= 10;
-//             resetsugarShards(sugarShard);
-//         }
-
-//         // advance the shards
-//         sugarShard.y += sugarShard.speed;
-
-//         // if the shard is below the canvas,
-//         if (cupCakes.y > canvas.height) {
-//             resetcupCakes(cupCakes);
-//         }
-
-//     }
-
-//     // redraw everything
-//     drawAll();
-
-
-
-// }
-
-// function isColliding(a, b) {
-//     return !(
-//     b.x > a.x + a.width || b.x + b.width < a.x || b.y > a.y + a.height || b.y + b.height < a.y);
-// }
-
-// function drawAll() {
-
-
-
-//     // draw all shards
-//     for (var i = 0; i < sugarShard.length; i++) {
-//         var sugarShard = sugarShard[i];
-        
-//         drawImage(sugarShardImage,sugarShard.x,sugarShard.y);
-
-
-  
-//     }
-//   }
--- js/working-code.js
+++ js/working-code.js
@@ -11,7 +11,15 @@ var canvasHeight = 1200;
 canvas.width = canvasWidth;
 canvas.height = canvasHeight; // need this for the canvas to show full
 
+// var Cat = function (){
+// this.y = 0;
+// this.x = 0;
+// this.img = '';
+// }
 
+// var cat = new Cat();
+// cat.x = 500;
+// cat.y = 600;
 var catImage = new Image();
 catImage.src = './images/cat.png'; //need this to show image
 
@@ -27,14 +35,14 @@ function drawCat(){
   ctx.drawImage(catImage,x,y)
 }
 
-function checkCollision(obstacle){
-  if((obstacle.y + (obstacle.height - halfOfYourObstacleHeight) >= y && obstacle.y <= y + theMainCharacter.height)
-  &&(obstacle.x + obstacle.width>= theMainCharacter.x &&obstacle.x <= theMainCharacter.x+theMainCharacter.width)){
+// function checkCollision(obstacle){
+//   if((obstacle.y + (obstacle.height - halfOfYourObstacleHeight) >= theMainCharacter.y && obstacle.y <= theMainCharacter.y + theMainCharacter.height)
+//   &&(obstacle.x + obstacle.width>= theMainCharacter.x &&obstacle.x <= theMainCharacter.x+theMainCharacter.width)){
 
-    console.log("crash");
-      //here you'll put the logic of getting points or losing lives, whichever your game has 
-  }}
-  checkCollision(thesugarShard);
+//     console.log("crash");
+//       //here you'll put the logic of getting points or losing lives, whichever your game has 
+//   }}
+//   checkCollision(thesugarShard)
 
 function updateCanvas(){
   setInterval(function(){
@@ -68,3 +76,84 @@ else if (e.which === 39){
 
 
 }; // do not remove
+
+
+
+// var canvas = document.getElementById('canvas');
+// var ctx = canvas.getContext('2d');
+// var canvasWidth = 1500;
+// var canvasHeight = 1200;
+// canvas.width = canvasWidth;
+// canvas.height = canvasHeight; // need this for the canvas to show full
+
+
+
+// //catImage.src = './images/cat.png'; //need this to show image
+
+//  var Cat = function (){
+//     this.x= 500;
+//     this.y= 600;
+//     this.width= 50;
+//     this.height= 50;
+//     this.img= './images/cat.png';
+//  }
+//  var cat = new Cat();
+//  var catImage = new Image();
+//  catImage.src = cat.img;
+ 
+// //  function updateFrame() {
+   
+// // } //need this to draw cat
+
+
+
+// function drawCat(){
+//   catImage.onload = function(){
+//   ctx.drawImage(catImage, cat.x, cat.y, cat.width, cat.height);
+//    } // updateCanvas();
+// }
+
+
+// document.onkeydown = function (e) {
+//   console.log("working moving!!!!");
+//   if (e.which === 38){
+//     cat.y = cat.y - 20;
+//   }
+//   else if (e.which === 40)
+//   {
+//     cat.y = cat.y + 20;
+//   }
+//   else if (e.which === 37){
+//     cat.x = cat.x - 20;
+//   }
+//   else if (e.which === 39){
+//     cat.x = cat.x + 20;
+//   }
+  
+  
+//   function updateCanvas(){
+//     setInterval(function(){
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   console.log("draw works");
+  
+//     drawCat();
+//     drawAllTheCupcakes();
+//     drawAllTheShards();
+//     },100)
+  
+//   }
+  // function checkCollision(obstacle){
+      //   if((obstacle.y + (obstacle.height - halfOfYourObstacleHeight) >= theMainCharacter.y && obstacle.y <= theMainCharacter.y + theMainCharacter.height)
+      //   &&(obstacle.x + obstacle.width>= theMainCharacter.x &&obstacle.x <= theMainCharacter.x+theMainCharacter.width)){
+        
+        //     console.log("crash");
+        //       //here you'll put the logic of getting points or losing lives, whichever your game has 
+        //   }}
+        //   checkCollision(thesugarShard)
+        
+        
+        
+        
+        
+      // }; // do not remove
+      
